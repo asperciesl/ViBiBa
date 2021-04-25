770f1cc9f726653cb0456515fd8f424042a27331
asperciesl <52851322+asperciesl@users.noreply.github.com>
2021-04-26 01:41:10 +0200

    bugfix

--- public/app/pages/sources/sources_overview.php
+++ public/app/pages/sources/sources_overview.php
@@ -7,14 +7,16 @@
         <hr/>
         <ul>
             <?php
-            foreach ($_SB->db_source_fetch_available()['id'] as $source) {
-                ?>
-                <li>
-                    <a href="/app/sources/upload/<?php echo $source['source_id']; ?>/">
-                        <?php echo '#' . $source['source_id'] . ' - ' . $source['source_name_' . $_SB->config()['languages']['default']]; ?>
-                    </a>
-                </li>
-                <?php
+            if (!empty($_SB->db_source_fetch_available())){
+                foreach ($_SB->db_source_fetch_available()['id'] as $source) {
+                    ?>
+                    <li>
+                        <a href="/app/sources/upload/<?php echo $source['source_id']; ?>/">
+                            <?php echo '#' . $source['source_id'] . ' - ' . $source['source_name_' . $_SB->config()['languages']['default']]; ?>
+                        </a>
+                    </li>
+                    <?php
+                }
             }
             ?>
         </ul>
@@ -30,14 +32,16 @@
         <hr/>
         <ul>
             <?php
-            foreach ($_SB->db_source_fetch_all()['id'] as $source) {
-                ?>
-                <li>
-                    <a href="/app/sources/view/<?php echo $source['source_id']; ?>/">
-                        <?php echo '#' . $source['source_id'] . ' - ' . $source['source_name_' . $_SB->config()['languages']['default']]; ?>
-                    </a>
-                </li>
-                <?php
+            if(!empty($_SB->db_source_fetch_all())){
+                foreach ($_SB->db_source_fetch_all()['id'] as $source) {
+                    ?>
+                    <li>
+                        <a href="/app/sources/view/<?php echo $source['source_id']; ?>/">
+                            <?php echo '#' . $source['source_id'] . ' - ' . $source['source_name_' . $_SB->config()['languages']['default']]; ?>
+                        </a>
+                    </li>
+                    <?php
+                }
             }
             ?>
         </ul>
