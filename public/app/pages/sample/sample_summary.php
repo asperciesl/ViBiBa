421b7dee815d1c377e418e830d108d92c55a3e6e
Jan-Philipp Cieslik <github@cieslik.io>
2021-06-28 20:03:27 +0200

    fixed sample summary

--- public/app/pages/sample/sample_summary.php
+++ public/app/pages/sample/sample_summary.php
@@ -16,7 +16,7 @@ $ous = $_SB->user->ou_fetch();
             <tr>
                 <th>Category</th>
                 <th>Type</th>
-                <th>Count</th>
+                <th>Sample Count</th>
                 <?php
                 $first_line = $summary[array_key_first($summary)];
                 $first_line = $first_line[array_key_first($first_line)];
